9e5edf9e3e0f70e70ffd9efc72d9beddd32befc6
zxh0 <zhangxiuhong2013@gmail.com>
2016-12-03 15:25:15 +0800

    fix issue #7

---
+++ v1/code/go/src/jvmgo/ch11/native/java/util/concurrent/atomic/AtomicLong.go
@@ -0,0 +1,12 @@
+package atomic
+
+import "jvmgo/ch11/native"
+import "jvmgo/ch11/rtda"
+
+func init() {
+	native.Register("java/util/concurrent/atomic/AtomicLong", "VMSupportsCS8", "()Z", vmSupportsCS8)
+}
+
+func vmSupportsCS8(frame *rtda.Frame) {
+	frame.OperandStack().PushBoolean(false)
+}
