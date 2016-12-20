daabcdc7a9953a66c1a9a7da505cfcc036b7e9e8
zxh0 <zhangxiuhong2013@gmail.com>
2016-12-20 10:40:49 +0800

    +SwitchTest.java

---
+++ v1/code/java/example/src/main/java/jvmgo/book/ch07/test/instructions/SwitchTest.java
@@ -0,0 +1,39 @@
+package jvmgo.book.ch07.test.instructions;
+
+import static jvmgo.book.ch07.test.Assert.*;
+
+public class SwitchTest {
+
+    public static void main(String[] args) {
+        assertTrue(chooseNear(0) == 0);
+        assertTrue(chooseNear(1) == 1);
+        assertTrue(chooseNear(2) == 2);
+        assertTrue(chooseNear(3) == -1);
+
+        assertTrue(chooseFar(-100) == -1);
+        assertTrue(chooseFar(0) == 0);
+        assertTrue(chooseFar(100) == 1);
+        assertTrue(chooseFar(12) == -1);
+    }
+
+    // tableswitch
+    private static int chooseNear(int i) {
+        switch (i) {
+            case 0:  return  0;
+            case 1:  return  1;
+            case 2:  return  2;
+            default: return -1;
+        }
+    }
+
+    // lookupswitch
+    private static int chooseFar(int i) {
+        switch (i) {
+            case -100: return -1;
+            case 0:    return  0;
+            case 100:  return  1;
+            default:   return -1;
+        }
+    }
+
+}
