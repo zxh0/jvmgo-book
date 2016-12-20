19821f7adf81a00a148cc414308ffaf91a793737
zxh0 <zhangxiuhong2013@gmail.com>
2016-12-20 10:41:02 +0800

    +Test.java

---
+++ v1/code/java/example/src/main/java/jvmgo/book/ch07/test/Test.java
@@ -0,0 +1,13 @@
+package jvmgo.book.ch07.test;
+
+import jvmgo.book.ch07.test.instructions.MathTest;
+import jvmgo.book.ch07.test.instructions.SwitchTest;
+
+public class Test {
+
+    public static void main(String[] args) {
+        MathTest.main(args);
+        SwitchTest.main(args);
+    }
+
+}
