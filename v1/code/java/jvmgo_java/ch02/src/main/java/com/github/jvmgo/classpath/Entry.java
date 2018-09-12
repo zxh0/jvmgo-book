6af74739cfbe21bf1d32343eab446c3312bc645b
zxh0 <zhangxiuhong2013@gmail.com>
2018-09-12 14:10:07 +0800

    + DirEntry

--- v1/code/java/jvmgo_java/ch02/src/main/java/com/github/jvmgo/classpath/Entry.java
+++ v1/code/java/jvmgo_java/ch02/src/main/java/com/github/jvmgo/classpath/Entry.java
@@ -3,6 +3,6 @@ package com.github.jvmgo.classpath;
 public interface Entry {
 
     // className: fully/qualified/ClassName.class
-    byte[] readClass(String className);
+    byte[] readClass(String className) throws Exception;
 
 }
