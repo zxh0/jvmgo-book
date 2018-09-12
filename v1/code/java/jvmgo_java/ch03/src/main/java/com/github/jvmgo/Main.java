ac5c24a5171e2b64e53c0f1bb6da27dc6a4b5170
zxh0 <zhangxiuhong2013@gmail.com>
2018-09-12 09:23:35 +0800

    + ch03 ~ ch11

---
+++ v1/code/java/jvmgo_java/ch03/src/main/java/com/github/jvmgo/Main.java
@@ -0,0 +1,9 @@
+package com.github.jvmgo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ v1/code/java/jvmgo_java/ch10/src/test/java/com/github/jvmgo/UnitTest.java
@@ -0,0 +1,13 @@
+package com.github.jvmgo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnitTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
