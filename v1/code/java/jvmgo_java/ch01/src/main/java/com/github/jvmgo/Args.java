791b7bafa0d6740668d24ddc731f528ff6193720
zxh0 <zhangxiuhong2013@gmail.com>
2018-09-12 13:27:52 +0800

    refactor Args ...

--- v1/code/java/jvmgo_java/ch01/src/main/java/com/github/jvmgo/Args.java
+++ v1/code/java/jvmgo_java/ch01/src/main/java/com/github/jvmgo/Args.java
@@ -1,6 +1,8 @@
 package com.github.jvmgo;
 
+import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 
 public class Args {
 
@@ -12,5 +14,22 @@ public class Args {
 
     @Parameter(names = { "-cp", "-classpath" }, description = "classpath")
     String classpath;
+
+    JCommander cmd;
+    boolean ok;
+
+    static Args parse(String[] argv) {
+        Args args = new Args();
+        args.cmd = JCommander.newBuilder()
+                .addObject(args)
+                .build();
+
+        try {
+            args.cmd.parse(argv);
+            args.ok = true;
+        } catch (ParameterException e) {
+            System.out.printf("Usage: <main class> [-options] class [args...]\n");
+        }
+    }
 
 }
--- v1/code/java/jvmgo_java/ch01/src/main/java/com/github/jvmgo/Main.java
+++ v1/code/java/jvmgo_java/ch01/src/main/java/com/github/jvmgo/Main.java
@@ -6,6 +6,8 @@ import com.beust.jcommander.ParameterException;
 public class Main {
 
     public static void main(String[] argv) {
+        Args args = Args.parse(argv);
+
         Args args = new Args();
         JCommander cmd = JCommander.newBuilder()
                 .addObject(args)
@@ -18,6 +20,9 @@ public class Main {
             return;
         }
 
+        if (! args.ok) {
+
+        }
         if (args.versionFlag) {
             System.out.println("java version \"1.8.0\"");
             return;
