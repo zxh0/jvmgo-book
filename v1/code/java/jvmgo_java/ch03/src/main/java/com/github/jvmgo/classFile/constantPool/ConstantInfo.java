19123e0bb9eb72ccbe397eb40db066ccb1277ee5
zxh0 <zhangxiuhong2013@gmail.com>
2018-10-12 16:00:12 +0800

    simplify ConstantInfo

--- v1/code/java/jvmgo_java/ch03/src/main/java/com/github/jvmgo/classFile/constantPool/ConstantInfo.java
+++ v1/code/java/jvmgo_java/ch03/src/main/java/com/github/jvmgo/classFile/constantPool/ConstantInfo.java
@@ -1,6 +1,5 @@
 package com.github.jvmgo.classfile.constantpool;
 
-import com.github.jvmgo.Main;
 import com.github.jvmgo.classfile.ClassReader;
 
 public interface ConstantInfo {
@@ -26,68 +25,47 @@ public interface ConstantInfo {
 
     //Factory method
     static ConstantInfo createConstantInfo(int tag, ClassReader reader, ConstantPool constPool) {
-
-        ConstantInfo constantInfo = null;
-
         switch (tag) {
-
             //数字
             case CONST_TAG_INTEGER:
-                constantInfo = new ConstantIntegerInfo(reader);
-                break;
+                return new ConstantIntegerInfo(reader);
             case CONST_TAG_FLOAT:
-                constantInfo = new ConstantFloatInfo(reader);
-                break;
+                return new ConstantFloatInfo(reader);
             case CONST_TAG_LONG:
-                constantInfo = new ConstantLongInfo(reader);
-                break;
+                return new ConstantLongInfo(reader);
             case CONST_TAG_DOUBLE:
-                constantInfo = new ConstantDoubleInfo(reader);
-                break;
+                return new ConstantDoubleInfo(reader);
 
             case CONST_TAG_UTF8:
-                constantInfo = new ConstUft8Info(reader);
-                break;
+                return new ConstUft8Info(reader);
 
             //以下3个引用UTF8
             case CONST_TAG_STRING:
-                constantInfo = new ConstStringInfo(constPool, reader);
-                break;
+                return new ConstStringInfo(constPool, reader);
             case CONST_TAG_CLASS:
-                constantInfo = new ConstClassInfo(constPool, reader);
-                break;
+                return new ConstClassInfo(constPool, reader);
             case CONST_TAG_NAME_AND_TYPE:
-                constantInfo = new ConstantNameAndTypeInfo(constPool, reader);
-                break;
+                return new ConstantNameAndTypeInfo(constPool, reader);
 
             //以下3个引用CLASS+NAME_AND_TYPE
             case CONST_TAG_FIELD_REF:
-                constantInfo = new ConstantMemberrefInfo(constPool, reader);
-                break;
+                return new ConstantMemberrefInfo(constPool, reader);
             case CONST_TAG_METHOD_REF:
-                constantInfo = new ConstantMemberrefInfo(constPool, reader);
-                break;
+                return new ConstantMemberrefInfo(constPool, reader);
             case CONST_TAG_INTERFACE_MTTHOD_REF:
-                constantInfo = new ConstantMemberrefInfo(constPool, reader);
-                break;
+                return new ConstantMemberrefInfo(constPool, reader);
 
             //是Java SE 7才添加到class文件中的，目的是支持新增的invokedynamic指令
             case CONST_TAG_METHOD_HANDLE:
-                constantInfo = null;
-                break;
+                return null;
             case CONST_TAG_METHOD_TYPE:
-                constantInfo = null;
-                break;
+                return null;
             case CONST_TAG_INVOKE_DYNAMIC:
-                constantInfo = null;
-                break;
+                return null;
 
             default:
-                Main.panic("java.lang.ClassFormatError: constant poll tag!");
-                break;
+                throw new ClassFormatError("constant pool tag!");
         }
-
-        return constantInfo;
     }
 
 }
