2746b91f49c967f3feeaf94e0c6cc662dd33a5c6
zxh0 <zhangxiuhong2013@gmail.com>
2018-10-12 16:22:26 +0800

    format Util

--- v1/code/java/jvmgo_java/ch03/src/main/java/com/github/jvmgo/util/Util.java
+++ v1/code/java/jvmgo_java/ch03/src/main/java/com/github/jvmgo/util/Util.java
@@ -1,27 +1,24 @@
 package com.github.jvmgo.util;
 
-/**
- * 刘欣那里复制的
- */
 public class Util {
-	public static int byteToInt(byte[] codes){
-    	String s1 = byteToHexString(codes);
-    	return Integer.valueOf(s1, 16).intValue();
+
+    public static int byteToInt(byte[] codes) {
+        String s1 = byteToHexString(codes);
+        return Integer.valueOf(s1, 16).intValue();
     }
-    
-    
-    
-	public static String byteToHexString(byte[] codes ){
-		StringBuffer buffer = new StringBuffer();
-		for(int i=0;i<codes.length;i++){
-			byte b = codes[i];
-			int value = b & 0xFF;
-			String strHex = Integer.toHexString(value);
-			if(strHex.length()< 2){
-				strHex = "0" + strHex;
-			}		
-			buffer.append(strHex);
-		}
-		return buffer.toString();
-	}
+
+    public static String byteToHexString(byte[] codes) {
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < codes.length; i++) {
+            byte b = codes[i];
+            int value = b & 0xFF;
+            String strHex = Integer.toHexString(value);
+            if (strHex.length() < 2) {
+                strHex = "0" + strHex;
+            }
+            buffer.append(strHex);
+        }
+        return buffer.toString();
+    }
+
 }
